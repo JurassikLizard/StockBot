33a9ce4e2330835300e7194fe30dfe9b1ad523ee
Andrey <al9999things@gmail.com>
2021-03-06 17:29:11 -0500

    Third Commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hi')
+from selenium import webdriver
+import time
+
+# declares chromedriver
+browser = webdriver.Chrome("C:\\Users\\Andrey\\Downloads\\chromedriver_win32\\chromedriver.exe")
+
+# open page
+browser.get("https://www.google.com/finance/quote/PEP:NASDAQ?window=5Y%22")
+time.sleep(1)
+# gets price
+price = browser.find_element_by_xpath("//div[@class='YMlKec fxKbKc']")
+print(price.text)
+quit()
+
+browser.find_element_by_tag_name("body")
